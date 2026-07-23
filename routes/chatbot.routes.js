af66415c2b3fe3b137902d943af722675ced12b4
Dilip492 <dpal99113@gmail.com>
2026-07-23 19:34:43 +0530

    fix the error chatbot file path error

--- routes/chatbot.routes.js
+++ routes/chatbot.routes.js
@@ -12,7 +12,7 @@ import { checkSubscription } from "../middleware/checkSubscriptions.js";
 import loadPlan from "../middleware/LoadPlan.js";
 import { getLeads } from "../controllers/widget.controller.js";
 import loadSubscription from "../middleware/LoadSubscriptions.js";
-import checkLimit from "../middleware/checklimit.js";
+import checkLimit from "../middleware/CheckLimit.js";
 
 const router = express.Router();
 
